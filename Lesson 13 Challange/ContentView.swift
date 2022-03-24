97b26f06f6079ae9358ff53a7b5b609e0a738158
raduliano <radulian@me.com>
2022-03-24 10:36:43 +0200

    Initial Commit

---
+++ Lesson 13 Challange/ContentView.swift
@@ -0,0 +1,81 @@
+//
+//  ContentView.swift
+//  Lesson 13 Challange
+//
+//  Created by Octav Radulian on 21.02.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    var myArray = ["apple", "cherry", "star"]
+    
+    @State var creditNumber = 1000
+    
+    @State var firstImage: String = "star"
+    @State var secondImage: String = "star"
+    @State var thirdImage: String = "star"
+    
+    
+    var body: some View {
+        
+        // Building the UI
+        VStack {
+            Spacer()
+            Text("SwiftUI Slots")
+            Spacer()
+            Text("Credits: \(creditNumber)")
+            Spacer()
+            HStack {
+                
+                Image(firstImage)
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    
+                Image(secondImage)
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                
+                Image(thirdImage)
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                
+                
+                
+            }
+            
+            Spacer()
+            
+            Button {
+                
+                firstImage = myArray.randomElement()!
+                secondImage = myArray.randomElement()!
+                thirdImage = myArray.randomElement()!
+                
+                if (firstImage == secondImage) && (secondImage == thirdImage) {
+                    creditNumber += 75
+                } else {
+                    creditNumber -= 10
+                }
+                
+                
+            } label: {
+                Text("Spin")
+                    .padding()
+                    .foregroundColor(.white)
+                    .background(.red)
+                    .cornerRadius(20)
+            }
+            
+            Spacer()
+
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Lesson 13 Challange/Lesson_13_ChallangeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Lesson_13_ChallangeApp.swift
+//  Lesson 13 Challange
+//
+//  Created by Octav Radulian on 21.02.2022.
+//
+
+import SwiftUI
+
+@main
+struct Lesson_13_ChallangeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
